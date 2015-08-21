d09a2e875f388769457fd0ad5568ce631e00a2e9
Renato Groffe <renato.groffe@yahoo.com.br>
2015-08-21 10:14:45 -0300

    Publicação inicial.

---
+++ TesteVS2015GitHub/ConversorTemperatura.cs
@@ -0,0 +1,15 @@
+﻿namespace TesteVS2015GitHub
+{
+    public static class ConversorTemperatura
+    {
+        public static double FahrenheitParaCelsius(double temperatura)
+        {
+            return 0;
+        }
+
+        public static double FahrenheitParaKelvin(double temperatura)
+        {
+            return 0;
+        }
+    }
+}
